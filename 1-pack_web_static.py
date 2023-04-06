4a0d9d021a8b8b518d54bf1dd3db535cc07624e5
Manuel254 <immanuelkituku@gmail.com>
2023-04-06 18:20:53 +0000

    Fix pep8 error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,7 @@ from fabric.api import *
 import os
 from datetime import datetime
 
+
 def do_pack():
     """This function generates a .tgz archive from
     the contents of the web_static folder
@@ -16,7 +17,8 @@ def do_pack():
     hour = datetime.now().hour
     mins = datetime.now().minute
     sec = datetime.now().second
+    comp = "tar -cvzf {}web_static_{}{:02d}{:02d}{:02d}{:02d}{:02d}.tgz {}"
 
     if not os.path.isdir(ver_dir):
         os.mkdir(ver_dir)
-    local("tar -cvzf {}web_static_{}{:02d}{:02d}{:02d}{:02d}{:02d}.tgz {}".format(ver_dir, year, month, day, hour, mins, sec, web_static)).success
+    local(comp.format(ver_dir, year, month, day, hour, mins, sec, web_static))
