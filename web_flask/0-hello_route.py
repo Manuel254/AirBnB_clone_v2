cadb3841f72014884b4fb2c85d43a2a5025c25bb
Manuel254 <immanuelkituku@gmail.com>
2023-04-20 09:12:25 +0000

    Added task 1 to 4 of flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This is a module that contains the flask app instance"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Renders the index page"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
