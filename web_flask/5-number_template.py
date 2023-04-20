0c1a647a7cdb162c6e626181e4a5047bb43f443f
Manuel254 <immanuelkituku@gmail.com>
2023-04-20 12:26:55 +0000

    Add task 4 and 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """This is a module that contains the flask app instance"""
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -44,6 +44,13 @@ def number(n):
     """Displays 'n is a number' only if n is an integer"""
     if isinstance(n, int):
         return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Displays a Html page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template('5-number.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
